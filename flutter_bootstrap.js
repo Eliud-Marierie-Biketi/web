273bbd21c4708fd4b69abcbd45498fc8d33ae23e
Eliud-Marierie-Biketi <elmariebiks03@gmail.com>
2025-08-12 13:28:32 +0300

    login spinner

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"1425e5e9ec5eeb4f225c401d8db69b860e0fde
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3172495421"
+    serviceWorkerVersion: "2964970858"
   }
 });
